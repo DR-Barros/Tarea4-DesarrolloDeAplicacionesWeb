1a048b0c9d63a77a8fb5946d008162b6dac8dee4
Daniel <danielradriganb@gmail.com>
2023-12-04 21:09:24 -0300

    FEAT(validacion): se crea clase para validarHincha

--- src/main/java/com/tarea4/panamericanos/controllers/ApiController.java
+++ src/main/java/com/tarea4/panamericanos/controllers/ApiController.java
@@ -3,8 +3,11 @@ package com.tarea4.panamericanos.controllers;
 import com.tarea4.panamericanos.services.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,5 +26,17 @@ public class ApiController {
     @GetMapping("/artesano-data")
     public Map<String, Integer> artesanoData() {
         return apiService.getCountTipoArtesanias();
+    }
+
+    @PostMapping("/post-hinchas")
+    public String postHinchas(@RequestParam String region,
+                              @RequestParam String comuna,
+                              @RequestParam("deportes") List<String> deportes,
+                              @RequestParam String transporte,
+                              @RequestParam String name,
+                              @RequestParam String mail,
+                              @RequestParam String phone,
+                              @RequestParam String coment) {
+        return "Exito";
     }
 }
